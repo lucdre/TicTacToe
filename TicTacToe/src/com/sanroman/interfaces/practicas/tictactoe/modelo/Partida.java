4661f2cbf31d0d9c87cab97cf745480558524c41
lucdre <ldtdamsanroman@gmail.com>
2015-01-29 14:09:19 +0100

    Created partida

---
+++ TicTacToe/src/com/sanroman/interfaces/practicas/tictactoe/modelo/Partida.java
@@ -0,0 +1,40 @@
+package com.sanroman.interfaces.practicas.tictactoe.modelo;
+
+/**
+ * 
+ * @since 29/01/2015 (Java 1.8)
+ * @author Luca Thiel (Lucdre)
+ *
+ */
+public class Partida {
+	
+	/**
+	 * De quien es el turno, true - circulo, false - X
+	 */
+	private boolean turno; 
+	
+	private Tablero tablero;
+	
+	/**
+	 * 
+	 * @param x - Alto del tablero
+	 * @param y - Ancho del tablero
+	 * @return True - Hay 3 en raya
+	 * @return False - No hay 3 en raya
+	 */
+	public boolean buscarTresEnRaya(int x, int y) {
+		
+		
+		
+//		for (int i = 0; i < tablero.getTablero().length; i++) {
+//			for (int j = 0; j < tablero.getTablero().length; j++) {
+//				
+//			}
+//		}
+		
+		return true;
+		
+	}
+	
+
+}
