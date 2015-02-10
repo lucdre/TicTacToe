6803f8c12e22f7db8ede56e8ef53ae421ee7d31b
lucdre <ldtdamsanroman@gmail.com>
2015-02-10 13:52:43 +0100

    Basic methods, still empty

--- src/com/sanroman/interfaces/practicas/tictactoe/controlador/Controlador.java
+++ src/com/sanroman/interfaces/practicas/tictactoe/controlador/Controlador.java
@@ -1,10 +1,26 @@
 package com.sanroman.interfaces.practicas.tictactoe.controlador;
 
+import com.sanroman.interfaces.practicas.tictactoe.modelo.Ficha;
+
 /**
  * @since 09/02/2015 (Java 1.8)
  * @author Luca
  *
  */
 public class Controlador {
+	
+	public void iniciar(){
+		
+	}
+	
+	public void cambiaTurno(){
+		
+	}
+	
+	public Ficha getTurno(){
+		
+	}
+	
+	
 
 }
